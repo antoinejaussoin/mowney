2788437acef950bbc6bfbb4647ac7c8d8e4ecf81
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2017-07-30 11:07:52 +0100

    Upgrade to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const languages = require('./app/i18n/languages');
 
@@ -10,7 +10,6 @@ const staticFolder = path.resolve(__dirname, 'assets');
 const momentFilter = languages.map(lang => lang.iso).join('|');
 
 module.exports = {
-    content: __dirname,
     entry: [
         'react-hot-loader/patch',
         './app/index.jsx'
@@ -20,6 +19,7 @@ module.exports = {
         publicPath: 'http://localhost:8080/assets/',
         filename: 'app.js'
     },
+    devtool: 'eval-source-map',
     devServer: {
         historyApiFallback: true,
         proxy: {
@@ -29,43 +29,42 @@ module.exports = {
             }
         }
     },
-    devtool: 'eval-source-map',
     resolve: {
-        extensions: ['', '.js', '.jsx'],
-        modulesDirectories: [
+        extensions: ['.js', '.jsx'],
+        modules: [
+            path.resolve('./app'),
             'node_modules',
             path.resolve(__dirname, './node_modules')
-        ],
-        root: [
-            path.resolve('./app')
         ]
     },
     module: {
-        preLoaders: [
-            { test: /(\.jsx|\.js)$/, loaders: ['eslint'] }
-        ],
-        loaders: [
-            { test: /\.css$/, loader: 'style!css' },
-            { test: /(\.jsx|\.js)$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.png$/, loader: 'url?limit=10000&mimetype=image/png' },
-            { test: /\.jpg$/, loader: 'url?limit=10000&mimetype=image/jpeg' },
-            { test: /\.json$/, loader: 'json-loader' },
-            { test: /(\.scss)$/, loader: 'style!css?sourceMap&modules&importLoaders=1&' +
-                'localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox' }
+        rules: [
+            { enforce: 'pre', test: /(\.jsx|\.js)$/, use: ['eslint-loader'] },
+
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /(\.jsx|\.js)$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.svg$/, loader: 'url-loader?limit=10' },
+            { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
+            { test: /\.jpg$/, loader: 'url-loader?limit=10000&mimetype=image/jpeg' },
+            {
+                test: /(\.scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                    {
+                        loader: 'sass-loader?sourceMap',
+                        options: {
+                            data: `@import "${path.resolve(__dirname, 'app/theme.scss')}";`
+                        }
+                    }
+                ]
+            }
         ]
-    },
-    toolbox: {
-        theme: path.join(__dirname, 'app/theme.scss')
-    },
-    postcss: [autoprefixer],
-    eslint: {
-        configFile: './.eslintrc',
-        emitWarning: true
     },
     plugins: [
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, new RegExp(momentFilter)),
-        new ExtractTextPlugin('style.css', { allChunks: true }),
-        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
+        new webpack.NoEmitOnErrorsPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development'),
             __DEVELOPMENT__: true,
