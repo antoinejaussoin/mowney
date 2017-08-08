fe06f1a70ddd061066053222def153995d9b587b
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2017-08-08 14:33:25 +0100

    Various

--- app/modules/app/components/Menu.jsx
+++ app/modules/app/components/Menu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { IconButton } from 'components/Button';
 import style from './Menu.scss';
 
 const Menu = () => (
   <div className={style.container}>
-    <Link to={'/upload'}>Upload</Link>
+    <Link to={'/upload'}><IconButton icon="file_upload" /></Link>
   </div>
 );
 
