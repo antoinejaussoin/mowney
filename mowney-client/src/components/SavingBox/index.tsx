b6e16d9b263d7804dcc9311380a089222a32fb01
Antoine Jaussoin <antoine.jaussoin@venn.twosigma.com>
2019-01-23 15:20:47 +0000

    Various

--- mowney-client/src/components/SavingBox/index.tsx
+++ mowney-client/src/components/SavingBox/index.tsx
@@ -13,10 +13,29 @@ interface ISavingItemProps {
   saving: GQL.ISavingPerRange;
 }
 
+const getLabel = (range: GQL.Range) => {
+  switch (range) {
+    case "currentMonth":
+      return "Current Month";
+    case "inception":
+      return "Inception";
+    case "lastMonth":
+      return "Last Month";
+    case "oneYear":
+      return "One Year";
+    case "sixMonth":
+      return "Six Months";
+    case "threeYears":
+      return "Three Years";
+    default:
+      return "<?>";
+  }
+};
+
 const SavingItem: SFC<ISavingItemProps> = ({ saving }) => (
   <Container positive={(saving.amountPerMonth || 0) >= 0}>
     <Header positive={(saving.amountPerMonth || 0) >= 0}>
-      <h4>{saving.range}</h4>
+      <h4>{getLabel(saving.range)}</h4>
       <div>{moment(saving.from!).format("DD/MM/YYYY")}</div>
       <div>{moment(saving.to!).format("DD/MM/YYYY")}</div>
     </Header>
@@ -26,23 +45,28 @@ const SavingItem: SFC<ISavingItemProps> = ({ saving }) => (
   </Container>
 );
 
-const Container = styled<{ positive: boolean }, "div">("div")`
+interface IPositiveProps {
+  positive: boolean;
+}
+
+const Container = styled<IPositiveProps, "div">("div")`
   min-width: 200px;
   border: 1px solid ${props => (props.positive ? colorPositive : colorNegative)};
   text-align: center;
   margin: 5px;
 `;
 
-const Header = styled<{ positive: boolean }, "div">("div")`
+const Header = styled<IPositiveProps, "div">("div")`
   border-bottom: 1px solid
     ${props => (props.positive ? colorPositive : colorNegative)};
 
   h4 {
     font-size: 1.5em;
+    margin: 5px;
   }
 `;
 
-const Content = styled<{ positive: boolean }, "div">("div")`
+const Content = styled<IPositiveProps, "div">("div")`
   background-color: ${props =>
     props.positive ? colorPositiveLight : colorNegativeLight};
   color: ${props => (props.positive ? colorPositive : colorNegative)};
